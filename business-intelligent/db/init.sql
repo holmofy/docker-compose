181082a0985dec0f2b64cfbfbca5f19c01fcb3fe
胡飞飞 <hu.feifei@darcytech.com>
2021-03-23 17:20:55 +0800

    business intelligence

---
+++ business-intelligent/db/init.sql
@@ -0,0 +1,5 @@
+create user superset with SUPERUSER password 'superset';
+create database superset owner superset;
+
+create user redash with SUPERUSER password 'passwd';
+create database redash owner redash;
